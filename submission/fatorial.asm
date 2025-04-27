ba74be60d02783c63816e03999d7a528d04c1aea
brunolima2696 <130502055+brunolima2696@users.noreply.github.com>
2025-04-27 20:05:57 -0300

    Update fatorial.asm

--- submission/fatorial.asm
+++ submission/fatorial.asm
@@ -1,27 +1,24 @@
-; Programa principal
-MAIN        SC FAT_SUB         ; Chama sub-rotina
-            HM /0000           ; Fim da execução
+MAIN        SC FAT_SUB        
+            HM /0000           
 
-; Sub-rotina iterativa de fatorial
+
 @ /200
 FAT_SUB     K /0000
-LOOP        LD N               ; Carrega N
-            JZ FIM_FAT         ; Se N == 0, termina
+LOOP        LD N               
+            JZ FIM_FAT         
 
-            ML RES             ; RES = RES * N
+            ML RES             
             MM RES
 
             LD N
-            SB UM              ; N = N - 1
+            SB UM              
             MM N
 
-            JP LOOP            ; Volta ao início da sub-rotina
+            JP LOOP            
 FIM_FAT     RS FAT_SUB
 
-; Variáveis e constantes
+
 @ /100
-N           K /0000            ; Argumento (n)
-
-RES         K /0001            ; Resultado (n!)
-
-UM          K /0001            ; Constante 1
+N           K /0000            
+RES         K /0001            
+UM          K /0001            
